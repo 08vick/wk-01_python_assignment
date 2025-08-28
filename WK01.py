ef6b4b02b5993f632a8c7fb6c37870f10ebbad28
08vick <107022719+08vick@users.noreply.github.com>
2025-08-28 21:22:21 +0300

    Update WK01.py

--- WK01.py
+++ WK01.py
@@ -1,4 +1,3 @@
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter an operation (+, -, *, /): ")
@@ -20,4 +19,5 @@ elif operation == '/':
     else:
         print("Error: Division by zero is not allowed.")
 else:
+
     print("Invalid operation. Please enter one of +, -, *, /")
